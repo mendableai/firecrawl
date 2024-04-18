c627d2217921e1e75ba992f2c8b82f03334ed35f
rafaelsideguide <150964962+rafaelsideguide@users.noreply.github.com>
2024-04-18 17:41:23 -0300

    all working now

--- apps/api/src/__tests__/e2e/index.test.ts
+++ apps/api/src/__tests__/e2e/index.test.ts
@@ -74,7 +74,7 @@ describe('E2E Tests for API Routes', () => {
         .set('Content-Type', 'application/json')
         .send({ url: 'https://firecrawl.dev' });
       expect(response.statusCode).toBe(200);
-      expect(response.body).toHaveProperty('asudhaopsidjp');
+      expect(response.body).toHaveProperty('jobId');
       expect(response.body.jobId).toMatch(/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/);
     });
 
