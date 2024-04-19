9e9d66f7a3979e3fbc179959bb35d8dc6233f091
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-04-20 02:27:53 +0900

    refactor: fix typo in WebScraper/index.ts
    breakign -> breaking

--- apps/api/src/scraper/WebScraper/index.ts
+++ apps/api/src/scraper/WebScraper/index.ts
@@ -352,7 +352,7 @@ export class WebScraperDataProvider {
       options.crawlerOptions?.generateImgAltText ?? false;
     this.pageOptions = options.pageOptions ?? {onlyMainContent: false};
 
-    //! @nicolas, for some reason this was being injected and breakign everything. Don't have time to find source of the issue so adding this check
+    //! @nicolas, for some reason this was being injected and breaking everything. Don't have time to find source of the issue so adding this check
     this.excludes = this.excludes.filter((item) => item !== "");
 
     // make sure all urls start with https://
