d9da4b53f89e26f600a0093ca30aaf01e773e04c
Nicolas <nicolascamara29@gmail.com>
2024-05-08 17:36:40 -0700

    Update example.py

--- apps/python-sdk/example.py
+++ apps/python-sdk/example.py
@@ -3,15 +3,16 @@ from firecrawl import FirecrawlApp
 
 app = FirecrawlApp(api_key="fc-YOUR_API_KEY")
 
-# crawl_result = app.crawl_url('mendable.ai', {'crawlerOptions': {'excludes': ['blog/*']}})
+crawl_result = app.crawl_url('mendable.ai', {'crawlerOptions': {'excludes': ['blog/*']}})
 
-# print(crawl_result[0]['markdown'])
+print(crawl_result[0]['markdown'])
 
-# job_id = crawl_result['jobId']
-# print(job_id)
+job_id = crawl_result['jobId']
+print(job_id)
 
-# status = app.check_crawl_status(job_id)
-# print(status)
+status = app.check_crawl_status(job_id)
+print(status)
+
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
