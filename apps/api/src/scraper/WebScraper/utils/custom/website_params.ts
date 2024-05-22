e4573c08cabde7477ca0e3d5440513b1d5505361
rafaelsideguide <150964962+rafaelsideguide@users.noreply.github.com>
2024-05-22 11:24:48 -0300

    Update website_params.ts

--- apps/api/src/scraper/WebScraper/utils/custom/website_params.ts
+++ apps/api/src/scraper/WebScraper/utils/custom/website_params.ts
@@ -137,6 +137,21 @@ export const urlSpecificParams = {
         "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     },
   },
+  "spider.cloud":{
+    defaultScraper: "fetch",
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+      "sec-fetch-site": "same-origin",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-dest": "empty",
+      referer: "https://www.google.com/",
+      "accept-language": "en-US,en;q=0.9",
+      "accept-encoding": "gzip, deflate, br",
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    },
+  },
   "help.salesforce.com":{
     defaultScraper: "playwright",
     params: {
