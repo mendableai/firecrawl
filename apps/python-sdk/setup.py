cd7f260288de324eb11bc6a84a6e020782cb0246
Matt Joyce <matt.joyce@gmail.com>
2024-05-26 11:33:28 +1000

    Added PyPi classifiers
    These classifiers will help potential users find and understand the purpose and status of the project.  use python 3.8 as the base, because that's what module 'requests' needs.

--- apps/python-sdk/setup.py
+++ apps/python-sdk/setup.py
@@ -1,20 +1,42 @@
 from pathlib import Path
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 
 this_directory = Path(__file__).parent
 long_description_content = (this_directory / "README.md").read_text()
 
 setup(
-    name='firecrawl-py',
-    version='0.0.9',
-    url='https://github.com/mendableai/firecrawl',
-    author='Mendable.ai',
-    author_email='nick@mendable.ai',
-    description='Python SDK for Firecrawl API',
+    name="firecrawl-py",
+    version="0.0.9",
+    url="https://github.com/mendableai/firecrawl",
+    author="Mendable.ai",
+    author_email="nick@mendable.ai",
+    description="Python SDK for Firecrawl API",
     long_description=long_description_content,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
-        'requests',
+        "requests",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Internet",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing",
+        "Topic :: Text Processing :: Indexing",
     ],
 )
