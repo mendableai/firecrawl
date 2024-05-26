e5c6ac23fe521f19de8c20ea8caceaade5c7c9a4
Matt Joyce <matt.joyce@gmail.com>
2024-05-26 10:01:35 +1000

    Added long description to PyPi
    https://packaging.python.org/en/latest/guides/making-a-pypi-friendly-readme/

--- apps/python-sdk/setup.py
+++ apps/python-sdk/setup.py
@@ -1,4 +1,8 @@
+from pathlib import Path
 from setuptools import setup, find_packages
+
+this_directory = Path(__file__).parent
+long_description_content = (this_directory / "README.md").read_text()
 
 setup(
     name='firecrawl-py',
@@ -7,6 +11,8 @@ setup(
     author='Mendable.ai',
     author_email='nick@mendable.ai',
     description='Python SDK for Firecrawl API',
+    long_description=long_description_content,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
         'requests',
