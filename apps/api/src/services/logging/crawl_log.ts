70be97b25f71e1ab84831fee6432099daf319517
Matt Joyce <matt.joyce@gmail.com>
2024-06-16 18:18:44 +1000

    fix supabase logging error message for self-host
    Calls to superbase fail if no superbase service
    USE_DB_AUTHENTICATION=False is our proxy for checking.

--- apps/api/src/services/logging/crawl_log.ts
+++ apps/api/src/services/logging/crawl_log.ts
@@ -2,16 +2,18 @@ import { supabase_service } from "../supabase";
 import "dotenv/config";
 
 export async function logCrawl(job_id: string, team_id: string) {
-  try {
-    const { data, error } = await supabase_service
-      .from("bulljobs_teams")
-      .insert([
-        {
-          job_id: job_id,
-          team_id: team_id,
-        },
-      ]);
-  } catch (error) {
-    console.error("Error logging crawl job:\n", error);
+  if(process.env.USE_DB_AUTHENTICATION){  // Only log to the database if the USE_DB_AUTHENTICATION environment variable is set to true
+    try {
+      const { data, error } = await supabase_service
+        .from("bulljobs_teams")
+        .insert([
+          {
+            job_id: job_id,
+            team_id: team_id,
+          },
+        ]);
+    } catch (error) {
+      console.error("Error logging crawl job:\n", error);
+    }
   }
 }
