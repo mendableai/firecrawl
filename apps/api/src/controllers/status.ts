e3279274f1d5c085aa5dfa6a634987a1cd49d51f
Gergő Móricz <mo.geryy@gmail.com>
2024-08-15 19:14:32 +0200

    fix: make playground crawl work

--- apps/api/src/controllers/status.ts
+++ apps/api/src/controllers/status.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { Logger } from "../../src/lib/logger";
 import { getCrawl, getCrawlJobs } from "../../src/lib/crawl-redis";
 import { getScrapeQueue } from "../../src/services/queue-service";
+import { supabaseGetJobById } from "../../src/lib/supabase-jobs";
 
 export async function crawlJobStatusPreviewController(req: Request, res: Response) {
   try {
@@ -21,7 +22,19 @@ export async function crawlJobStatusPreviewController(req: Request, res: Respons
     //   }
     // }
 
-    const jobs = await Promise.all(jobIDs.map(x => getScrapeQueue().getJob(x)));
+    const jobs = (await Promise.all(jobIDs.map(async x => {
+      const job = await getScrapeQueue().getJob(x);
+      
+      if (process.env.USE_DB_AUTHENTICATION === "true") {
+        const supabaseData = await supabaseGetJobById(job.id);
+
+        if (supabaseData) {
+          job.returnvalue = supabaseData.docs;
+        }
+      }
+
+      return job;
+    }))).sort((a, b) => a.timestamp - b.timestamp);
     const jobStatuses = await Promise.all(jobs.map(x => x.getState()));
     const jobStatus = sc.cancelled ? "failed" : jobStatuses.every(x => x === "completed") ? "completed" : jobStatuses.some(x => x === "failed") ? "failed" : "active";
 
