861e2ebdf1f4f38cec6c2f3b1b6e599647469128
Nicolas <nicolascamara29@gmail.com>
2024-08-27 12:08:50 -0300

    Nick: 2x rate limits

--- apps/api/src/services/rate-limiter.ts
+++ apps/api/src/services/rate-limiter.ts
@@ -18,9 +18,22 @@ const RATE_LIMITS = {
   },
   scrape: {
     default: 20,
+    free: 10,
+    starter: 20,
+    standard: 100,
+    standardOld: 40,
+    scale: 500,
+    hobby: 20,
+    standardNew: 100,
+    standardnew: 100,
+    growth: 1000,
+    growthdouble: 1000,
+  },
+  search: {
+    default: 20,
     free: 5,
     starter: 20,
-    standard: 50,
+    standard: 40,
     standardOld: 40,
     scale: 500,
     hobby: 10,
@@ -29,7 +42,7 @@ const RATE_LIMITS = {
     growth: 500,
     growthdouble: 500,
   },
-  search: {
+  map:{
     default: 20,
     free: 5,
     starter: 20,
