2444f7c450acac23f0b512ad4a33f4e15e448f35
Nicolas <nicolascamara29@gmail.com>
2024-09-02 18:51:18 -0300

    Update scrape.ts

--- apps/api/src/controllers/v1/scrape.ts
+++ apps/api/src/controllers/v1/scrape.ts
@@ -147,6 +147,6 @@ export async function scrapeController(
   return res.status(200).json({
     success: true,
     data: legacyDocumentConverter(doc),
-    scrape_id: origin === "website" ? jobId : undefined,
+    scrape_id: origin?.includes("website") ? jobId : undefined,
   });
 }
