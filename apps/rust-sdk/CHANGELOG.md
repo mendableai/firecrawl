4f091496686d251c56bcc8c2d3c344274afc1af7
emreboun <abdurrahmanemreyilmaz@gmail.com>
2024-09-02 07:08:01 +0300

    overage code removed, docker files updated, concurrent npm script added

--- apps/rust-sdk/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-## CHANGELOG
-
-## [0.1]
-
-### Added
-
-- [feat] Firecrawl rust sdk.
--- apps/test-suite/utils/types.ts
+++
@@ -1,7 +0,0 @@
-export interface WebsiteScrapeError {
-  website: string;
-  prompt: string;
-  expected_output: string;
-  actual_output: string;
-  error: string;
-}
