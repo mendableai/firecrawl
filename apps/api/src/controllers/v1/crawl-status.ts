eb03a81152883b5371b2a4519f98bae1d71065dd
Nicolas <nicolascamara29@gmail.com>
2024-09-05 12:55:04 -0300

    Update crawl-status.ts

--- apps/api/src/controllers/v1/crawl-status.ts
+++ apps/api/src/controllers/v1/crawl-status.ts
@@ -94,7 +94,8 @@ export async function crawlStatusController(req: RequestWithAuth<CrawlStatusPara
 
   const data = doneJobs.map(x => x.returnvalue);
 
-  const nextURL = new URL(`${req.protocol}://${req.get("host")}/v1/crawl/${req.params.jobId}`);
+  const protocol = process.env.ENV === "local" ? req.protocol : "https";
+  const nextURL = new URL(`${protocol}://${req.get("host")}/v1/crawl/${req.params.jobId}`);
 
   nextURL.searchParams.set("skip", (start + data.length).toString());
 
