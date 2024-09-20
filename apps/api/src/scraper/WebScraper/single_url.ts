e1a34b0a99521f19356f6e13d304e45be56c69fc
Gergő Móricz <mo.geryy@gmail.com>
2024-09-20 21:43:22 +0200

    Revert "feat(scrape): scroll down/up with actions if fullpagescreenshot"
    This reverts commit 815bfc8f07b3493ed91ff90afda123e7d84f44f9.

--- apps/api/src/scraper/WebScraper/single_url.ts
+++ apps/api/src/scraper/WebScraper/single_url.ts
@@ -210,26 +210,10 @@ export async function scrapSingleUrl(
                   type: "wait" as const,
                   milliseconds: pageOptions.waitFor,
                 }] : []),
-                ...(pageOptions.fullPageScreenshot ? [
-                  {
-                    type: "scroll" as const,
-                    direction: "down" as const,
-                  },
-                  {
-                    type: "wait" as const,
-                    milliseconds: 300,
-                  },
-                ] : []),
                 ...((pageOptions.screenshot || pageOptions.fullPageScreenshot) ? [{
                   type: "screenshot" as const,
                   fullPage: !!pageOptions.fullPageScreenshot,
                 }] : []),
-                ...(pageOptions.fullPageScreenshot ? [
-                  {
-                    type: "scroll" as const,
-                    direction: "up" as const,
-                  }
-                ] : []),
                 ...(pageOptions.actions ?? []),
               ],
             }) : ({
