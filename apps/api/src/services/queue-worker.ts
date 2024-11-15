ca2e33db0ac5c138427d9ef0f0c74498d76efe2b
Gergő Móricz <mo.geryy@gmail.com>
2024-11-15 19:55:23 +0100

    fix(log_job): add force option to retry on supabase failure

--- apps/api/src/services/queue-worker.ts
+++ apps/api/src/services/queue-worker.ts
@@ -346,7 +346,7 @@ async function processJob(job: Job & { id: string }, token: string) {
         scrapeOptions: job.data.scrapeOptions,
         origin: job.data.origin,
         crawl_id: job.data.crawl_id,
-      });
+      }, true);
 
       await addCrawlJobDone(job.data.crawl_id, job.id);
 
@@ -486,7 +486,7 @@ async function processJob(job: Job & { id: string }, token: string) {
             url: sc?.originUrl ?? (job.data.crawlerOptions === null ? "Batch Scrape" : "Unknown"),
             crawlerOptions: sc.crawlerOptions,
             origin: job.data.origin,
-          });
+          }, true);
         }
       }
     }
@@ -566,7 +566,7 @@ async function processJob(job: Job & { id: string }, token: string) {
         scrapeOptions: job.data.scrapeOptions,
         origin: job.data.origin,
         crawl_id: job.data.crawl_id,
-      });
+      }, true);
 
       // await logJob({
       //   job_id: job.data.crawl_id,
