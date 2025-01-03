92b46e784a17d1c72130cb6830a39893a5978eb3
Gergő Móricz <mo.geryy@gmail.com>
2025-01-03 15:03:20 +0100

    feat: new snips test framework

--- apps/api/src/scraper/scrapeURL/engines/fire-engine/delete.ts
+++ apps/api/src/scraper/scrapeURL/engines/fire-engine/delete.ts
@@ -2,8 +2,9 @@ import { Logger } from "winston";
 import * as Sentry from "@sentry/node";
 
 import { robustFetch } from "../../lib/fetch";
+import { MockState } from "../../lib/mock";
 
-export async function fireEngineDelete(logger: Logger, jobId: string) {
+export async function fireEngineDelete(logger: Logger, jobId: string, mock: MockState | null) {
   const fireEngineURL = process.env.FIRE_ENGINE_BETA_URL!;
 
   await Sentry.startSpan(
@@ -28,6 +29,7 @@ export async function fireEngineDelete(logger: Logger, jobId: string) {
         ignoreResponse: true,
         ignoreFailure: true,
         logger: logger.child({ method: "fireEngineDelete/robustFetch", jobId }),
+        mock,
       });
     },
   );
