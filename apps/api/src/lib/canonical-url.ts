f25c0c6d216c3242b114d5fdada4b67a74d4e08c
Nicolas <nicolascamara29@gmail.com>
2025-01-03 23:16:33 -0300

    Nick: added canonical tests

--- apps/api/src/lib/canonical-url.ts
+++ apps/api/src/lib/canonical-url.ts
@@ -1,7 +1,8 @@
 export function normalizeUrl(url: string) {
-  url = url.replace(/^https?:\/\//, "").replace(/^www\./, "");
-  if (url.endsWith("/")) {
-    url = url.slice(0, -1);
+  try {
+    const hostname = new URL(url).hostname;
+    return hostname.replace(/^www\./, "");
+  } catch (error) {
+    return url.replace(/^https?:\/\//, "").replace(/^www\./, "").split('/')[0];
   }
-  return url;
 }
