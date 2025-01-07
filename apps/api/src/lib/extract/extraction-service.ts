7a7882af77fed8a044093359d5f84117d28ca739
rafaelmmiller <150964962+rafaelsideguide@users.noreply.github.com>
2025-01-07 18:58:51 -0300

    wip

--- apps/api/src/lib/extract/extraction-service.ts
+++ apps/api/src/lib/extract/extraction-service.ts
@@ -89,7 +89,6 @@ async function analyzeSchemaAndPrompt(numUrls: number, schema: any, prompt: stri
   });
 
   const { hasLargeArrays, keysWithLargeArrays } = checkSchema.parse(result.choices[0].message.parsed);
-  console.log({ hasLargeArrays, keysWithLargeArrays });
   return { hasLargeArrays, keysWithLargeArrays };
 }
 
@@ -137,12 +136,8 @@ export async function performExtraction(extractId: string, options: ExtractServi
   // if so, it splits the results into 2 types of completions:
   // 1. the first one is a completion that will extract the array of items
   // 2. the second one is multiple completions that will extract the items from the array
+  const { hasLargeArrays, keysWithLargeArrays } = await analyzeSchemaAndPrompt(links.length, request.schema, request.prompt ?? "");
   
-  // const { hasLargeArrays, keysWithLargeArrays } = await analyzeSchemaAndPrompt(links.length, request.schema, request.prompt ?? "");
-  const hasLargeArrays = true;
-  const keysWithLargeArrays = ["products"];
-  
-  console.log({ hasLargeArrays, keysWithLargeArrays });
   if (hasLargeArrays) {
     // removes from reqSchema the keys that are large arrays and adds them to largeArraysSchema
     for (const key of keysWithLargeArrays) {
@@ -225,10 +220,10 @@ export async function performExtraction(extractId: string, options: ExtractServi
     //   v1: true,
     // }, {}, crypto.randomUUID(), 50);
 
-    //we restructure and make all of the arrays we need to fill into objects, 
+    // we restructure and make all of the arrays we need to fill into objects, 
     // adding them to a single object so the llm can fill them one at a time
-    //TODO: make this work for more complex schemas where arrays are not first level
-    console.log("schema", largeArraysSchema);
+    // TODO: make this work for more complex schemas where arrays are not first level
+    
     let schemasForLLM: {} = {};
     for(const key in largeArraysSchema) {
       const originalSchema = structuredClone(largeArraysSchema[key].items);
@@ -247,158 +242,72 @@ export async function performExtraction(extractId: string, options: ExtractServi
       schemasForLLM[key] = clonedObj;
     }
 
-    console.log("schemasForLLM",schemasForLLM);
-    for (const key in largeArraysSchema) {
-      console.log('>>>>>>', key, largeArraysSchema[key]);
-    }
+    const timeout = Math.floor((request.timeout || 40000) * 0.7) || 30000;
+    const scrapePromises = links.map(url =>
+      scrapeDocument({
+        url,
+        teamId,
+        plan,
+        origin: request.origin || "api",
+        timeout,
+      }, urlTraces)
+    );
 
-    // for (const key in largeArraysSchema) {
-      // console.log('>>>>>>', key, largeArraysSchema[key]);
-      // Scrape documents
-      const timeout = Math.floor((request.timeout || 40000) * 0.7) || 30000;
-      const scrapePromises = links.map(url =>
-        scrapeDocument({
-          url,
-          teamId,
-          plan,
-          origin: request.origin || "api",
-          timeout,
-        }, urlTraces)
+    let docs = (await Promise.all(scrapePromises)).filter((doc): doc is Document => doc !== null);
+
+    for (const doc of docs) {
+      const schema = {properties:schemasForLLM,type:"object"};
+      ajv.compile(schema);
+      // Generate completions
+      const comp = await generateOpenAICompletions(
+        logger.child({ method: "extractService/generateOpenAICompletions" }),
+        {
+          mode: "llm",
+          systemPrompt:
+            (request.systemPrompt ? `${request.systemPrompt}\n` : "") +
+            "Always prioritize using the provided content to answer the question. Do not make up an answer. Do not hallucinate. Be concise and follow the schema always if provided. Here are the urls the user provided of which he wants to extract information from: " +
+            links.join(", "),
+          prompt: request.prompt,
+          schema: schemasForLLM,
+        },
+        buildDocument(doc),
+        undefined,
+        true,
       );
-
-      let docs = (await Promise.all(scrapePromises)).filter((doc): doc is Document => doc !== null);
-
-      for (const doc of docs) {
-
-        const schema = {properties:schemasForLLM,type:"object"};
-        console.log("schemaa",schema);
-        console.log("schema",JSON.stringify(schema));
-        ajv.compile(schema);
-
-
-
-
-
-
+      
+      // Update token usage in traces
+      if (comp && comp.numTokens) {
+        const totalLength = docs.reduce((sum, doc) => sum + (doc.markdown?.length || 0), 0);
+        docs.forEach((doc) => {
+          if (doc.metadata?.sourceURL) {
+            const trace = urlTraces.find((t) => t.url === doc.metadata.sourceURL);
+            if (trace && trace.contentStats) {
+              trace.contentStats.tokensUsed = Math.floor(
+                ((doc.markdown?.length || 0) / totalLength) * (comp?.numTokens || 0)
+              );
+            }
+          }
+        });
         
-        // console.log(doc, key, largeArraysSchema[key]);
-        // Generate completions
-        const comp = await generateOpenAICompletions(
-          logger.child({ method: "extractService/generateOpenAICompletions" }),
-          {
-            mode: "llm",
-            systemPrompt:
-              (request.systemPrompt ? `${request.systemPrompt}\n` : "") +
-              "Always prioritize using the provided content to answer the question. Do not make up an answer. Do not hallucinate. Be concise and follow the schema always if provided. Here are the urls the user provided of which he wants to extract information from: " +
-              links.join(", "),
-            prompt: request.prompt,
-            schema: schemasForLLM,
-          },
-          buildDocument(doc),
-          undefined,
-          true,
-        );
-        console.log("llm response",JSON.stringify(comp, null, 2));
-        // Update token usage in traces
-        if (comp && comp.numTokens) {
-
-          const totalLength = docs.reduce((sum, doc) => sum + (doc.markdown?.length || 0), 0);
-          docs.forEach((doc) => {
-            if (doc.metadata?.sourceURL) {
-              const trace = urlTraces.find((t) => t.url === doc.metadata.sourceURL);
-              if (trace && trace.contentStats) {
-                trace.contentStats.tokensUsed = Math.floor(
-                  ((doc.markdown?.length || 0) / totalLength) * (comp?.numTokens || 0)
-                );
-              }
-            }
-          });
-          console.log("extract", JSON.stringify(comp));
-          for(const [key, value] of Object.entries(comp.extract)){
+        for (const [key, value] of Object.entries(comp.extract)){
+          //@ts-ignore
+          if(value.informationFilled === true){
+            let res = value;
             //@ts-ignore
-            if(value.informationFilled === true){
-              let res = value;
-              //@ts-ignore
-              delete res.informationFilled;
-              if (!largeArrayResult[key]) {
-                largeArrayResult[key] = [];
-              }
-              //@ts-ignore
-              largeArrayResult[key].push(value.data);
-
+            delete res.informationFilled;
+            if (!largeArrayResult[key]) {
+              largeArrayResult[key] = [];
             }
+            //@ts-ignore
+            largeArrayResult[key].push(value.data);
+
           }
         }
       }
-
-
-      console.log("Results!!!!, ",largeArrayResult);
-  
-
-    //   for (const doc of docs) {
-    //     console.log(doc, key, largeArraysSchema[key]);
-    //     // Generate completions
-    //     const comp = await generateOpenAICompletions(
-    //       logger.child({ method: "extractService/generateOpenAICompletions" }),
-    //       {
-    //         mode: "llm",
-    //         systemPrompt:
-    //           (request.systemPrompt ? `${request.systemPrompt}\n` : "") +
-    //           "Always prioritize using the provided content to answer the question. Do not make up an answer. Do not hallucinate. Be concise and follow the schema always if provided. Here are the urls the user provided of which he wants to extract information from: " +
-    //           links.join(", "),
-    //         prompt: request.prompt,
-    //         schema: largeArraysSchema[key],
-    //       },
-    //       buildDocument(doc),
-    //       undefined,
-    //       true,
-    //     );
-    //     console.log(JSON.stringify(comp, null, 2));
-    //     // Update token usage in traces
-    //     if (comp && comp.numTokens) {
-    //       const totalLength = docs.reduce((sum, doc) => sum + (doc.markdown?.length || 0), 0);
-    //       docs.forEach((doc) => {
-    //         if (doc.metadata?.sourceURL) {
-    //           const trace = urlTraces.find((t) => t.url === doc.metadata.sourceURL);
-    //           if (trace && trace.contentStats) {
-    //             trace.contentStats.tokensUsed = Math.floor(
-    //               ((doc.markdown?.length || 0) / totalLength) * (comp?.numTokens || 0)
-    //             );
-    //           }
-    //         }
-    //       });
-    //     }
-    //   }
-
-
-
-    // //   // merge the results into the 
-    // //   const keys = key.split('.');
-    // //   let currentLevel = largeArrayResult;
-
-    // //   // Traverse or create nested objects based on the keys
-    // //   for (let i = 0; i < keys.length; i++) {
-    // //     const part = keys[i];
-    // //     if (i === keys.length - 1) {
-    // //       // Assign the array to the last key
-    // //       currentLevel[part] = comp.extract[part]; // TODO: replace with actual results
-    // //     } else {
-    // //       // Create the object if it doesn't exist
-    // //       if (!currentLevel[part]) {
-    // //         currentLevel[part] = {};
-    // //       }
-    // //       currentLevel = currentLevel[part];
-    // //     }
-    // //   }
-    // }
+    }
   }
   
   if (reqSchema && Object.keys(reqSchema).length > 0) {
-
-    // Perform item extraction
-    // const itemCompletions = await generateItemCompletions(request, docs);
-    // Process itemCompletions as needed
-
     // Scrape documents
     const timeout = Math.floor((request.timeout || 40000) * 0.7) || 30000;
     const scrapePromises = links.map(url =>
@@ -430,7 +339,7 @@ export async function performExtraction(extractId: string, options: ExtractServi
         mode: "llm",
         systemPrompt:
           (request.systemPrompt ? `${request.systemPrompt}\n` : "") +
-          "Always prioritize using the provided content to answer the question. Do not make up an answer. Do not hallucinate. Be concise and follow the schema always if provided. Here are the urls the user provided of which he wants to extract information from: " +
+          "Always prioritize using the provided content to answer the question. Do not make up an answer. Do not hallucinate. Return 'null' if you cannot find the information. Be concise and follow the schema always if provided. Here are the urls the user provided of which he wants to extract information from: " +
           links.join(", "),
         prompt: request.prompt,
         schema: request.schema,
@@ -454,62 +363,6 @@ export async function performExtraction(extractId: string, options: ExtractServi
         }
       });
     }
-
-    // Kickoff background crawl for indexing root domains
-    // const rootDomains = new Set(request.urls.map(getRootDomain));
-    // rootDomains.forEach(async url => {
-    //   const crawlId = crypto.randomUUID();
-      
-    //   // Create and save crawl configuration first
-    //   const sc: StoredCrawl = {
-    //     originUrl: url,
-    //     crawlerOptions: {
-    //       maxDepth: 15,
-    //       limit: 5000,
-    //       includePaths: [],
-    //       excludePaths: [],
-    //       ignoreSitemap: false,
-    //       includeSubdomains: true,
-    //       allowExternalLinks: false,
-    //       allowBackwardLinks: true
-    //     },
-    //     scrapeOptions: {
-    //         formats: ["markdown"],
-    //         onlyMainContent: true,
-    //         waitFor: 0,
-    //         mobile: false,
-    //         removeBase64Images: true,
-    //         fastMode: false,
-    //         parsePDF: true,
-    //         skipTlsVerification: false,
-    //     },
-    //     internalOptions: { 
-    //       disableSmartWaitCache: true,
-    //       isBackgroundIndex: true
-    //     },
-    //     team_id: process.env.BACKGROUND_INDEX_TEAM_ID!,
-    //     createdAt: Date.now(),
-    //     plan: "hobby", // make it a low concurrency
-    //   };
-
-    //   // Save the crawl configuration
-    //   await saveCrawl(crawlId, sc);
-
-    //   // Then kick off the job
-    //   await _addScrapeJobToBullMQ({
-    //     url,
-    //     mode: "kickoff" as const,
-    //     team_id: process.env.BACKGROUND_INDEX_TEAM_ID!,
-    //     plan: "hobby", // make it a low concurrency
-    //     crawlerOptions: sc.crawlerOptions,
-    //     scrapeOptions: sc.scrapeOptions,
-    //     internalOptions: sc.internalOptions,
-    //     origin: "index",
-    //     crawl_id: crawlId,
-    //     webhook: null,
-    //     v1: true,
-    //   }, {}, crypto.randomUUID(), 50);
-    // });
   }
 
   if (completions && completions.extract) {
