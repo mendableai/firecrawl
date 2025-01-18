88904a5cded39dd5fa9c319a12696a9bf5925806
Nicolas <nicolascamara29@gmail.com>
2025-01-18 18:10:23 -0300

    Nick: js-sdk 1.15.4

--- apps/js-sdk/firecrawl/src/index.ts
+++ apps/js-sdk/firecrawl/src/index.ts
@@ -1262,7 +1262,7 @@ export default class FirecrawlApp {
       throw new FirecrawlError(
         `Failed to ${action}. Status code: ${response.status}. Error: ${errorMessage}${details}`,
         response.status,
-        response.data.details
+        response?.data?.details
       );
     } else {
       throw new FirecrawlError(
