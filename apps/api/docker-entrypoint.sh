ce4efb6606c379e15ff67c5b901a35d9279f4121
Nick Roth <nlr06886@gmail.com>
2025-02-04 12:22:39 -0600

    Add ulimit back to docker-entrypoint.sh
    This implements the proposed approach to not fail when the ulimit command can't run due to the environment

--- apps/api/docker-entrypoint.sh
+++ apps/api/docker-entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -e
 
+if [ $UID -eq 0 ]; then
+  set +e # disable failing on errror
+  ulimit -n 65535
+  echo "NEW ULIMIT: $(ulimit -n)"
+  set -e # enable failing on error
+else
+  echo ENTRYPOINT DID NOT RUN AS ROOT
+fi
+
 if [ $FLY_PROCESS_GROUP = "app" ]; then
   echo "RUNNING app"
   node --max-old-space-size=8192 dist/src/index.js
