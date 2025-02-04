0848b1ac603ae853aac95915f3183e3c48d7103f
Nick Roth <nlr06886@gmail.com>
2025-02-04 09:45:08 -0600

    Remove ulimit from entrypoint

--- apps/api/docker-entrypoint.sh
+++ apps/api/docker-entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -e
 
-if [ $UID -eq 0 ]; then
-  ulimit -n 65535
-  echo "NEW ULIMIT: $(ulimit -n)"
-else
-  echo ENTRYPOINT DID NOT RUN AS ROOT
-fi
-
 if [ $FLY_PROCESS_GROUP = "app" ]; then
   echo "RUNNING app"
   node --max-old-space-size=8192 dist/src/index.js
