15489be542b2fe2ace992209f79b94504a61bc78
Gergő Móricz <mo.geryy@gmail.com>
2025-02-23 09:07:32 +0100

    feat(self-host/ai): use any OpenAI-compatible API (#1245)

--- apps/api/src/lib/generic-ai.ts
+++ apps/api/src/lib/generic-ai.ts
@@ -1,9 +1,12 @@
-import { openai } from '@ai-sdk/openai';
+import { createOpenAI } from '@ai-sdk/openai';
 import { createOllama } from "ollama-ai-provider";
 
 const modelAdapter = process.env.OLLAMA_BASE_URL ? createOllama({
-    baseURL: process.env.OLLAMA_BASE_URL!,
-}) : openai;
+    baseURL: process.env.OLLAMA_BASE_URL,
+}) : createOpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    baseURL: process.env.OPENAI_BASE_URL,
+});
 
 export function getModel(name: string) {
     return process.env.MODEL_NAME ? modelAdapter(process.env.MODEL_NAME) : modelAdapter(name);
