415603acb0587e98aa66addb3ff9eb89827ef1b2
Gergő Móricz <mo.geryy@gmail.com>
2025-04-10 12:50:03 +0200

    fixes

--- apps/api/src/scraper/scrapeURL/transformers/diff.ts
+++ apps/api/src/scraper/scrapeURL/transformers/diff.ts
@@ -1,22 +1,29 @@
 import { supabase_service } from "../../../services/supabase";
 import { Document } from "../../../controllers/v1/types";
 import { Meta } from "../index";
+import { getJob } from "../../../controllers/v1/crawl-status";
 
 export async function deriveDiff(meta: Meta, document: Document): Promise<Document> {
   if (meta.options.formats.includes("changeTracking")) {
     const res = await supabase_service
-        .rpc("diff_get_last_scrape_1", {
+        .rpc("diff_get_last_scrape_2", {
             i_team_id: meta.internalOptions.teamId,
             i_url: document.metadata.sourceURL ?? meta.url,
         });
 
     const data: {
-        o_docs: Document[],
+        o_job_id: string,
         o_date_added: string,
     } | undefined | null = (res.data ?? [])[0] as any;
 
-    if (data && data.o_docs.length > 0) {
-        const previousMarkdown = data.o_docs[0].markdown!;
+    const job: {
+        returnvalue: Document,
+    } | null = data?.o_job_id ? await getJob(data.o_job_id) : null;
+
+    console.log(data, job);
+
+    if (data && job && job?.returnvalue) {
+        const previousMarkdown = job.returnvalue.markdown!;
         const currentMarkdown = document.markdown!;
 
         const transformer = (x: string) => [...x.replace(/\s+/g, "").replace(/\[iframe\]\(.+?\)/g, "")].sort().join("");
