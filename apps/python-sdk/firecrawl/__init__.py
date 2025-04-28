8053a7cedde1653e496dde9d8fa6050bfcf8d478
Nicolas <nicolascamara29@gmail.com>
2025-04-28 15:01:12 -0300

    Nick: updates on pypi

--- apps/python-sdk/firecrawl/__init__.py
+++ apps/python-sdk/firecrawl/__init__.py
@@ -13,7 +13,7 @@ import os
 
 from .firecrawl import FirecrawlApp, JsonConfig, ScrapeOptions # noqa
 
-__version__ = "2.4.0"
+__version__ = "2.4.3"
 
 # Define the logger for the Firecrawl project
 logger: logging.Logger = logging.getLogger("firecrawl")
