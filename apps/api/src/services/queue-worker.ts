2ac101753814de51ce06500d5fb89ad3df264435
Nicolas <nicolascamara29@gmail.com>
2025-05-15 19:11:58 -0300

    Update queue-worker.ts

--- apps/api/src/services/queue-worker.ts
+++ apps/api/src/services/queue-worker.ts
@@ -1366,7 +1366,7 @@ async function processJob(job: Job & { id: string }, token: string) {
           creditsToBeBilled = 150;
         }
       } else if (doc.metadata.numPages !== undefined && doc.metadata.numPages > 1) {
-        const creditsPerPDFPage = 5;
+        const creditsPerPDFPage = 1;
         creditsToBeBilled = creditsPerPDFPage * doc.metadata.numPages;
       }
 
