b8703b2a720765b92f5c4cab94cc90ea624198a8
Gergő Móricz <mo.geryy@gmail.com>
2025-05-16 15:27:24 +0200

    feat: use cacheable lookup everywhere (#1559)
    * feat(scrapeURL): use cacheableLookup
    
    * feat(queue-worker): add cacheablelookup
    
    * fix(cacheable-lookup): make it work with tailscale on local
    
    * add devenv
    
    * try again
    
    * allow querying all
    
    * log
    
    * fixes
    
    * asd
    
    * fix:
    
    * fix(lookup):

--- apps/api/src/services/queue-worker.ts
+++ apps/api/src/services/queue-worker.ts
@@ -82,6 +82,9 @@ import { performExtraction_F0 } from "../lib/extract/fire-0/extraction-service-f
 import { CostTracking } from "../lib/extract/extraction-service";
 import { getACUCTeam } from "../controllers/auth";
 import Express from "express";
+import http from "http";
+import https from "https";
+import { cacheableLookup } from "../scraper/scrapeURL/lib/cacheableLookup";
 
 configDotenv();
 
@@ -108,6 +111,10 @@ const connectionMonitorInterval =
 const gotJobInterval = Number(process.env.CONNECTION_MONITOR_INTERVAL) || 20;
 
 const runningJobs: Set<string> = new Set();
+
+// Install cacheable lookup for all other requests
+cacheableLookup.install(http.globalAgent);
+cacheableLookup.install(https.globalAgent);
 
 async function finishCrawlIfNeeded(job: Job & { id: string }, sc: StoredCrawl) {
   const logger = _logger.child({
