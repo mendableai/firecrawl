34ca4a69bb2eed70db249542e66413c9d9c64d28
Gergő Móricz <mo.geryy@gmail.com>
2025-05-16 14:58:16 +0200

    try again

--- apps/api/src/scraper/scrapeURL/lib/cacheableLookup.ts
+++ apps/api/src/scraper/scrapeURL/lib/cacheableLookup.ts
@@ -2,5 +2,5 @@ import CacheableLookup from 'cacheable-lookup';
 
 export const cacheableLookup = new CacheableLookup({
   // this is important to avoid querying local hostnames see https://github.com/szmarczak/cacheable-lookup readme
-  ...(process.env.ENV !== "production" || process.env.SENTRY_ENVIRONMENT === "dev" ? {} : { lookup: false }),
+  ...((process.env.ENV !== "production" || process.env.SENTRY_ENVIRONMENT === "dev") ? {} : { lookup: false }),
 });
