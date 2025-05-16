5302345ad30490997eae977344dd29b3a9e71e26
Gergő Móricz <mo.geryy@gmail.com>
2025-05-16 15:16:50 +0200

    asd

--- apps/api/src/scraper/scrapeURL/lib/cacheableLookup.ts
+++ apps/api/src/scraper/scrapeURL/lib/cacheableLookup.ts
@@ -2,7 +2,7 @@ import CacheableLookup from 'cacheable-lookup';
 
 export const cacheableLookup = new CacheableLookup({});
 
-if (process.env.ENV === "test") {
+if (process.env.ENV === "production" && process.env.SENTRY_ENVIRONMENT === "dev") {
     console.log(cacheableLookup.servers);
     cacheableLookup.servers.push("100.100.100.100");
 }
