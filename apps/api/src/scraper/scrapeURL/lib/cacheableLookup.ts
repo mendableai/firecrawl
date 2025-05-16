6a4f2ee7a789aed867f44d7f89f5d1f4067f3587
Gergő Móricz <mo.geryy@gmail.com>
2025-05-16 15:13:53 +0200

    fixes

--- apps/api/src/scraper/scrapeURL/lib/cacheableLookup.ts
+++ apps/api/src/scraper/scrapeURL/lib/cacheableLookup.ts
@@ -1,3 +1,8 @@
 import CacheableLookup from 'cacheable-lookup';
 
 export const cacheableLookup = new CacheableLookup({});
+
+if (process.env.ENV === "test") {
+    console.log(cacheableLookup.servers);
+    cacheableLookup.servers.push("100.100.100.100");
+}
