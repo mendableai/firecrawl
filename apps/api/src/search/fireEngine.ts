9297afd1ff11d1b9fb539a02b836fdc707844249
Nicolas <nicolascamara29@gmail.com>
2025-05-29 17:00:13 -0300

    Nick: search

--- apps/api/src/search/fireEngine.ts
+++ apps/api/src/search/fireEngine.ts
@@ -5,6 +5,58 @@ import { logger } from "../lib/logger";
 
 dotenv.config();
 
+
+export async function fire_engine_search(
+  q: string,
+  options: {
+    tbs?: string;
+    filter?: string;
+    lang?: string;
+    country?: string;
+    location?: string;
+    numResults: number;
+    page?: number;
+  },
+  abort?: AbortSignal,
+): Promise<SearchResult[]> {
+  try {
+    let data = JSON.stringify({
+      query: q,
+      lang: options.lang,
+      country: options.country,
+      location: options.location,
+      tbs: options.tbs,
+      numResults: options.numResults,
+      page: options.page ?? 1,
+    });
+
+    if (!process.env.FIRE_ENGINE_BETA_URL) {
+      return [];
+    }
+
+    const response = await fetch(`${process.env.FIRE_ENGINE_BETA_URL}/search`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Disable-Cache": "true",
+      },
+      body: data,
+      signal: abort,
+    });
+
+    if (response.ok) {
+      const responseData = await response.json();
+      return responseData;
+    } else {
+      return [];
+    }
+  } catch (error) {
+    logger.error(error);
+    Sentry.captureException(error);
+    return [];
+  }
+}
+
 export async function fireEngineMap(
   q: string,
   options: {
@@ -34,7 +86,7 @@ export async function fireEngineMap(
       return [];
     }
 
-    const response = await fetch(`${process.env.FIRE_ENGINE_BETA_URL}/search`, {
+    const response = await fetch(`${process.env.FIRE_ENGINE_BETA_URL}/map`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
