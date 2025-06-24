52b6cd0a0e5a5903c6580e49cc6565dbb2a0fb1e
Gergő Móricz <mo.geryy@gmail.com>
2025-06-24 13:26:44 +0200

    fix: bad merge

--- apps/api/src/__tests__/snips/lib.ts
+++ apps/api/src/__tests__/snips/lib.ts
@@ -10,26 +10,77 @@ import request from "supertest";
 
 const TEST_URL = "http://127.0.0.1:3002";
 
-export type Identity = {
-    apiKey: string;
-}
-
-export const defaultIdentity: Identity = {
-    apiKey: process.env.TEST_API_KEY!,
-};
-
 // Due to the limited resources of the CI runner, we need to set a longer timeout for the many many scrape tests
 export const scrapeTimeout = 75000;
 export const indexCooldown = 30000;
 
 // =========================================
+// idmux
+// =========================================
+
+export type IdmuxRequest = {
+    name: string,
+
+    concurrency?: number,
+    credits?: number,
+    tokens?: number,
+    flags?: any,
+}
+
+export async function idmux(req: IdmuxRequest): Promise<Identity> {
+    if (!process.env.IDMUX_URL) {
+        console.warn("IDMUX_URL is not set, using test API key and team ID");
+        return {
+            apiKey: process.env.TEST_API_KEY!,
+            teamId: process.env.TEST_TEAM_ID!,
+        }
+    }
+
+    const res = await fetch(process.env.IDMUX_URL + "/", {
+        method: "POST",
+        body: JSON.stringify({
+            refName: process.env.GITHUB_REF_NAME!,
+            runNumber: parseInt(process.env.GITHUB_RUN_NUMBER!),
+            concurrency: req.concurrency ?? 100,
+            ...req,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    expect(res.ok).toBe(true);
+    return await res.json();
+}
+
+export type Identity = {
+    apiKey: string;
+    teamId: string;
+}
+
+let defaultIdentity: Identity | null = null;
+
+export async function getDefaultIdentity(): Promise<Identity> {
+    if (!defaultIdentity) {
+        defaultIdentity = await idmux({
+            name: "default",
+
+            concurrency: 100,
+            credits: 1000000,
+            tokens: 1000000,
+        })
+    }
+    return defaultIdentity;
+}
+
+// =========================================
 // Scrape API
 // =========================================
 
-async function scrapeRaw(body: ScrapeRequestInput, identity = defaultIdentity) {
+async function scrapeRaw(body: ScrapeRequestInput, identity?: Identity) {
     return await request(TEST_URL)
         .post("/v1/scrape")
-        .set("Authorization", `Bearer ${identity.apiKey}`)
+        .set("Authorization", `Bearer ${(identity ?? await getDefaultIdentity()).apiKey}`)
         .set("Content-Type", "application/json")
         .send(body);
 }
@@ -163,7 +214,7 @@ export async function crawlErrors(id: string, identity?: Identity): Promise<Excl
     return res.body;
 }
 
-export async function crawl(body: CrawlRequestInput, identity = defaultIdentity): Promise<Exclude<CrawlStatusResponse, ErrorResponse>> {
+export async function crawl(body: CrawlRequestInput, identity?: Identity): Promise<Exclude<CrawlStatusResponse & { id: string }, ErrorResponse>> {
     const cs = await crawlStart(body, identity);
     expectCrawlStartToSucceed(cs);
 
@@ -219,7 +270,7 @@ function expectBatchScrapeToSucceed(response: Awaited<ReturnType<typeof batchScr
     expect(response.body.data.length).toBeGreaterThan(0);
 }
 
-export async function batchScrape(body: BatchScrapeRequestInput, identity = defaultIdentity): Promise<Exclude<CrawlStatusResponse, ErrorResponse>> {
+export async function batchScrape(body: BatchScrapeRequestInput, identity?: Identity): Promise<Exclude<CrawlStatusResponse, ErrorResponse> & { id: string }> {
     const bss = await batchScrapeStart(body, identity);
     expectBatchScrapeStartToSucceed(bss);
 
