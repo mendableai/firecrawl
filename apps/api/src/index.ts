392d2c0fb8cdeed3534d5c710cdaef89bc6154b6
Nicolas <nicolascamara29@gmail.com>
2025-07-24 17:22:16 -0300

    Nick: init

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -26,6 +26,7 @@ import { v4 as uuidv4 } from "uuid";
 import { RateLimiterMode } from "./types";
 import { attachWsProxy } from "./services/agentLivecastWS";
 import { cacheableLookup } from "./scraper/scrapeURL/lib/cacheableLookup";
+import { v2Router } from "./routes/v2";
 
 const { createBullBoard } = require("@bull-board/api");
 const { BullAdapter } = require("@bull-board/api/bullAdapter");
@@ -82,6 +83,7 @@ app.get("/test", async (req, res) => {
 // register router
 app.use(v0Router);
 app.use("/v1", v1Router);
+app.use("/v2", v2Router);
 app.use(adminRouter);
 
 const DEFAULT_PORT = process.env.PORT ?? 3002;
