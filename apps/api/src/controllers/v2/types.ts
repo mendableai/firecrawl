c3d8408df6453ac8fb8a528385b19daa45fca6d4
Gergő Móricz <mo.geryy@gmail.com>
2025-08-14 18:47:22 +0200

    scrape: auto proxy by default

--- apps/api/src/controllers/v2/types.ts
+++ apps/api/src/controllers/v2/types.ts
@@ -319,7 +319,7 @@ const baseScrapeOptions = z
     fastMode: z.boolean().default(false),
     useMock: z.string().optional(),
     blockAds: z.boolean().default(true),
-    proxy: z.enum(["basic", "stealth", "auto"]).optional(),
+    proxy: z.enum(["basic", "stealth", "auto"]).default("auto"),
     maxAge: z.number().int().gte(0).safe().default(2 * 24 * 60 * 60 * 1000), // 2 days
     storeInCache: z.boolean().default(true),
     // @deprecated
