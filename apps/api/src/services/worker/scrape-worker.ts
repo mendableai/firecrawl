1cca6d85378f10dd3d8f4a0596fa79b6ecf692bb
Gergő Móricz <mo.geryy@gmail.com>
2025-08-15 13:09:22 +0200

    feat(api): add OTEL everywhere

--- apps/api/src/services/worker/scrape-worker.ts
+++ apps/api/src/services/worker/scrape-worker.ts
@@ -48,6 +48,10 @@ import { finishCrawlIfNeeded } from "./crawl-logic";
 import { LangfuseExporter } from "langfuse-vercel";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { NodeSDK } from "@opentelemetry/sdk-node";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
+import { resourceFromAttributes } from "@opentelemetry/resources";
+import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
 
 class RacedRedirectError extends Error {
     constructor() {
@@ -849,21 +853,30 @@ export const processJobInternal = async (job: Job & { id: string }) => {
     }
 };
 
-const langfuseOtel = process.env.LANGFUSE_PUBLIC_KEY ? new NodeSDK({
-    traceExporter: new LangfuseExporter(),
+const shouldOtel = process.env.LANGFUSE_PUBLIC_KEY || process.env.OTEL_EXPORTER_OTLP_ENDPOINT;
+const otelSdk = shouldOtel ? new NodeSDK({
+    resource: resourceFromAttributes({
+        [ATTR_SERVICE_NAME]: "firecrawl-worker-scrape",
+    }),
+    spanProcessors: [
+        ...(process.env.LANGFUSE_PUBLIC_KEY ? [new BatchSpanProcessor(new LangfuseExporter())] : []),
+        ...(process.env.OTEL_EXPORTER_OTLP_ENDPOINT ? [new BatchSpanProcessor(new OTLPTraceExporter({
+            url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+        }))] : []),
+    ],
     instrumentations: [getNodeAutoInstrumentations()],
 }) : null;
     
-if (langfuseOtel) {   
-    langfuseOtel.start();
+if (otelSdk) {   
+    otelSdk.start();
 }
 
 module.exports = processJobInternal;
 
 const exitHandler = () => {
-    if (langfuseOtel) {
-        langfuseOtel.shutdown().then(() => {
-            _logger.debug("Langfuse OTEL shutdown");
+    if (otelSdk) {
+        otelSdk.shutdown().then(() => {
+            _logger.debug("OTEL shutdown");
             process.exit(0);
         });
     }
