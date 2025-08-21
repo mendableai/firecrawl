7c1726e11256b346f86faef6b5c8ccb6802232b0
Nicolas <nicolascamara29@gmail.com>
2025-08-20 18:28:20 -0700

    Update __init__.py

--- apps/python-sdk/firecrawl/__init__.py
+++ apps/python-sdk/firecrawl/__init__.py
@@ -17,7 +17,7 @@ from .v1 import (
     V1ChangeTrackingOptions,
 )
 
-__version__ = "3.0.4"
+__version__ = "3.1.0"
 
 # Define the logger for the Firecrawl project
 logger: logging.Logger = logging.getLogger("firecrawl")
