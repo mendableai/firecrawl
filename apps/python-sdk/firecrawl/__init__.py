54d8d92c9901bd3c3e7564c080017a230345251e
Nicolas <nicolascamara29@gmail.com>
2025-08-23 16:59:39 -0700

    Nick: sdks now have search categories

--- apps/python-sdk/firecrawl/__init__.py
+++ apps/python-sdk/firecrawl/__init__.py
@@ -17,7 +17,7 @@ from .v1 import (
     V1ChangeTrackingOptions,
 )
 
-__version__ = "3.3.0"
+__version__ = "3.3.2"
 
 # Define the logger for the Firecrawl project
 logger: logging.Logger = logging.getLogger("firecrawl")
